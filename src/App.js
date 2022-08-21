152df1490e6e446155bb2418bf76482662a82835
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2022-08-21 11:11:00 +0000

    make it better

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ export default function App() {
   return (
     <>
     <FormOn />
+    <div>
+    <table>
+      <tr>
+        <th></th>
+        <th>List of Users</th>
+        <th></th>
+      </tr>
+  <tr>
+    <th>#</th>
+    <th>Name</th>
+    <th>Contact</th>
+  </tr>
+  
+</table>
+    </div>
     </>
   );
 }
--- src/components/FormOn.js
+++ src/components/FormOn.js
@@ -1,24 +1,34 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
 import { Formik, Field, Form } from "formik";
 
 const FormOn = () => {
-  var data=[]
+  const [data, setData]=useState([])
+  
   return (
     <div className="App">
-      <h1>Contact Us</h1>
+      
       <Formik
         initialValues={{ name: "", email: "" }}
         onSubmit={async (values) => {
           await new Promise((resolve) => setTimeout(resolve, 500));
-          data.push(JSON.stringify(values, null, 2))
-          axios.post('https:sample-endpoint.com/user', {
-            Name: 'Fred',
-            Age: '23'
-          })
-          .then(function (response) {
-            console.log(response);
-          })
-          console.log(typeof(data[0]),data)
+          //user(values)
+          
+          data.push(values)
+        
+        
+          var ele=document.querySelector("#root > div:nth-child(2) > table")
+          ele.innerHTML=ele.innerHTML+`<tr>
+          <td>${data.length}</td>
+          <td>${values.name}</td>
+          <td>${values.Mobile}</td>
+        </tr>`
+       
+        
+          
+        console.clear();
+
+          console.log(data,)
         }}
       >
         <Form>
@@ -26,15 +36,15 @@ const FormOn = () => {
           <h1>Personal Details</h1>
             <div className="grid3">
             <div className="grid2">
-            <label className="imp">Name</label>
+            <label className="important">Name</label>
             <Field name="name" type="text" placeholder="Enter Name"/>
             </div>
             <div className="grid2">
-            <label className="imp">Date of Birth or Age</label>
+            <label className="important">Date of Birth or Age</label>
             <Field name="dob" type="text" placeholder="DD/MM/YY or Age in Years"/>
             </div>
             <div className="grid2">
-            <label className="imp">Sex</label>
+            <label className="important">Sex</label>
             <Field as="select" name="Sex">
             <option value="0" selected>Select Sex</option>
              <option value="Male">Male</option>
@@ -43,11 +53,11 @@ const FormOn = () => {
            </Field>
             </div>
             <div className="grid2">
-            <label className="imp">Mobile</label>
-            <Field name="Mobile" type="text" placeholder="Enter Mobile" />
+            <label>Mobile</label>
+            <Field name="Mobile" type="phone" placeholder="Enter Mobile" />
             </div>
             <div className="grid2">
-            <label className="imp">Govt Issued ID</label>
+            <label>Govt Issued ID</label>
             <Field as="select" name="ID">
             <option value="0" selected>ID Type</option>
              <option value="Male">Pan Card</option>
@@ -61,7 +71,7 @@ const FormOn = () => {
             <h1>Contact Details</h1>
             <div className="grid3">
             <div className="g3">
-            <label className="imp">Guardian Details</label>
+            <label>Guardian Details</label>
             <Field as="select" name="Guardian">
             <option value="0" selected>Enter Label</option>
              <option value="Male">Parent</option>
@@ -74,11 +84,11 @@ const FormOn = () => {
             
             
             <div className="grid2">
-            <label className="imp">Email</label>
+            <label>Email</label>
             <Field name="email" type="email" placeholder="Enter Email" />
             </div>
             <div className="grid2">
-            <label className="imp">Emergency Contact <br></br>Number</label>
+            <label>Emergency Contact <br></br>Number</label>
             <Field name="Emergency" type="phone" placeholder="Enter Emergency No."/>
             </div>
           
@@ -87,15 +97,15 @@ const FormOn = () => {
             
             <div className="grid3">
             <div className="grid2">
-            <label className="imp">Address</label>
+            <label>Address</label>
             <Field name="Address" type="text" placeholder="Enter Address"/>
             </div>
             <div className="grid2">
-            <label className="imp">State</label>
+            <label>State</label>
             <Field name="State" type="text" placeholder="Enter State"/>
             </div>
             <div className="grid2">
-            <label className="imp">City</label>
+            <label>City</label>
             <Field as="select" name="City">
             <option value="0" selected>Enter City/town/village</option>
              <option value="Male">Varanasi</option>
@@ -104,13 +114,13 @@ const FormOn = () => {
            </Field>
             </div>
             <div className="grid2">
-            <label className="imp">Country</label>
+            <label>Country</label>
             
-            <Field name="Country" type="search" value="India"/>
+            <Field name="Country" type="search" />
             
             </div>
             <div className="grid2">
-            <label className="imp">Pincode</label>
+            <label>Pincode</label>
             <Field name="Pincode" type="text" placeholder="Enter Pincode"/>
             </div>
           
@@ -119,11 +129,11 @@ const FormOn = () => {
             
             <div className="grid3">
             <div className="grid2">
-            <label className="imp">Occupation</label>
+            <label>Occupation</label>
             <Field name="Occupation" type="text" placeholder="Enter Occupation"/>
             </div>
             <div className="grid2">
-            <label className="imp">Religion</label>
+            <label>Religion</label>
             <Field as="select" name="Religion">
             <option value="0" selected>Enter Religion</option>
              <option value="married">Hindu</option>
@@ -132,7 +142,7 @@ const FormOn = () => {
            </Field>
             </div>
             <div className="grid2">
-            <label className="imp">Marital Status</label>
+            <label>Marital Status</label>
             <Field as="select" name="Marital">
             <option value="0" selected>Enter Marital Status</option>
              <option value="married">married</option>
@@ -141,7 +151,7 @@ const FormOn = () => {
            </Field>
             </div>
             <div className="grid2">
-            <label className="imp">Blood Group</label>
+            <label>Blood Group</label>
             <Field as="select" name="blood">
             <option value="0" selected>Group</option>
              <option value="A+">A+</option>
@@ -156,8 +166,8 @@ const FormOn = () => {
            </Field>
             </div>
             <div className="grid2">
-            <label className="imp">Nationality</label>
-            <Field name="Nationality" type="search" value="India"/>
+            <label>Nationality</label>
+            <Field name="Nationality" type="search" />
             </div>
           
             </div>
@@ -169,6 +179,7 @@ const FormOn = () => {
           
         </Form>
       </Formik>
+      
     </div>
   )
 }
